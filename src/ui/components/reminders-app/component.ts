6fd923c25bf6c42b62271baa669a6b590b73377f
Santiago Ferreira <san650@gmail.com>
2017-04-26 13:15:40 -0300

    Add new todos

--- src/ui/components/reminders-app/component.ts
+++ src/ui/components/reminders-app/component.ts
@@ -2,11 +2,19 @@ import Component, { tracked } from "@glimmer/component";
 
 export default class Reminders extends Component {
   @tracked newTodoText = ''
+  @tracked todos = [
+    'First item',
+    'Second item',
+    'Third item'
+  ]
 
   handleSubmit(event) {
     event.preventDefault();
 
-    console.log(this.newTodoText);
+    this.todos = [
+      ...this.todos,
+      this.newTodoText
+    ];
   }
 
   handleKeyUp(event) {

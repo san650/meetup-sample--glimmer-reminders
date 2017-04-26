563c151650321c3079a6b6a8be24cfea83617749
Santiago Ferreira <san650@gmail.com>
2017-04-26 13:12:48 -0300

    Wire new todo form

--- src/ui/components/reminders-app/component.ts
+++ src/ui/components/reminders-app/component.ts
@@ -1,5 +1,15 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from "@glimmer/component";
 
 export default class Reminders extends Component {
+  @tracked newTodoText = ''
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.newTodoText);
+  }
+
+  handleKeyUp(event) {
+    this.newTodoText = event.target.value;
+  }
 }
